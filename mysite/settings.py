b62c16b2fe610983b4efeb80a8a12cd320962620
hibiki <suzukih_pc@yahoo.co.jp>
2019-08-17 20:46:34 +0900

    My Django Girls app, debug true

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '^=3@peephw7h%^e9q88w2s&rkrg#g@gytr*k4a@&#0&xzrf%ax'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1', 'pythonanywhere.com']
 
